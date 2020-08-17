c50b4325df19fd30dc69a0111160b7e4f318f59c
prem prasad khanal <khnl.prem@gmail.com>
2020-08-17 16:51:22 +0545

    site dashabord

--- js/zite.js
+++ js/zite.js
@@ -111,28 +111,7 @@ var pageSection = $(".bg-image");
   customTab();
 
 
-$(".custom-progress").each(function() {
 
-  var value = $(this).attr('data-value');
-  var left = $(this).find('.progress-left .custom-progress-bar');
-  var right = $(this).find('.progress-right .custom-progress-bar');
-
-  if (value > 0) {
-    if (value <= 50) {
-      right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-    } else {
-      right.css('transform', 'rotate(180deg)')
-      left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
-    }
-  }
-
-})
-
-function percentageToDegrees(percentage) {
-
-  return percentage / 100 * 360
-
-}
 
 })(window.jQuery);
 
